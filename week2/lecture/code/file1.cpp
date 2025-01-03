173f10f5cbfa0e56d2b73b0dcef817d9fda86173
Ryan Baker <rbaker@xtier.com>
2025-01-03 11:10:17 -0500

    Drafted lecture 2 material

---
+++ week2/lecture/code/file1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "file2.hpp"
+
+int main()
+{
+    greet();
+}
---
+++ week2/lecture/code/file2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "file2.hpp"
+
+void greet()
+{
+    std::cout << "Hello from file2!" << std::endl;
+}
