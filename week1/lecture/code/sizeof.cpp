a1c26af837503187013dfb170fe3fe8b58034b0c
Ryan Baker <rbaker@xtier.com>
2025-01-03 17:41:21 -0500

    Completed lecture 1 material

---
+++ week1/lecture/code/sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "      sizeof(int): " << sizeof(int) << std::endl;
+    std::cout << "    sizeof(short): " << sizeof(short) << std::endl;
+    std::cout << "     sizeof(long): " << sizeof(long) << std::endl;
+    std::cout << "sizeof(long long): " << sizeof(long long) << std::endl;
+    std::cout << "   sizeof(signed): " << sizeof(signed) << std::endl;
+    std::cout << " sizeof(unsigned): " << sizeof(unsigned) << std::endl;
+    std::cout << std::endl
+    std::cout << "sizeof(char): " << sizeof(char) << std::endl;
+
+    return 0;
+}
